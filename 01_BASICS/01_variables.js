1b54f3bec5ff06cf63c7b28e73fd4d990db3a5e8
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-03-26 02:14:51 +0530

    learnt variables basics

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let accountName = "Surjo"
+var accountEmail = "surjosen400@gmail.com"
+accountCity = "Dhanbad"
+
+// accountId = 2345
+accountName = "Sekhar"
+accountEmail = "surjosen9905@gmail.com"
+accountCity = "Bengaluru"
+
+/* Never use var because it is old and it does not allow block scope and scoping of variables inside a block basically!!
+Use let instead!!..Even don't leave any variable without declaration with let or const!! */
+
+console.table([accountId, accountName, accountEmail, accountCity])
