c93c09ca3ccf4026e3cb77186d87fd29013ec9bd
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-03-26 21:48:08 +0530

    undefined variables

--- 01_BASICS/01_variables.js
+++ 01_BASICS/01_variables.js
@@ -7,8 +7,9 @@ accountCity = "Dhanbad"
 accountName = "Sekhar"
 accountEmail = "surjosen9905@gmail.com"
 accountCity = "Bengaluru"
+let accountState;
 
 /* Never use var because it is old and it does not allow block scope and scoping of variables inside a block basically!!
 Use let instead!!..Even don't leave any variable without declaration with let or const!! */
 
-console.table([accountId, accountName, accountEmail, accountCity])
+console.table([accountId, accountName, accountEmail, accountCity, accountState])
