b831750a64cee295c8d7987d97f761b97f5257cf
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-03-28 04:36:50 +0530

    studied operations on values

---
+++ 01_BASICS/04_operations.js
@@ -0,0 +1,24 @@
+"use strict";
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log("1"+ "2");
+console.log("1"+ 2 + 2);
+console.log(1+ 2 + "2");
+
+console.log(+true);
+console.log(+"");
+console.log(+"Surjo");
+console.log(+"3");
+
+let score = 20;
+console.log(++score);
+let scores = 20;
+console.log(scores++);
