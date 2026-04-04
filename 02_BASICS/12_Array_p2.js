40495145072e7701be6aef0b45372981a96d23d1
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-04-05 01:46:58 +0530

    studied about more methods of array and spread operator

---
+++ 02_BASICS/12_Array_p2.js
@@ -0,0 +1,40 @@
+const marvel_heroes = ["Spiderman", "thor", "hulk", "ironman"];
+const dc_heroes = ["batman", "flash", "superman", "wonderwoman"];
+
+const mixed = marvel_heroes.push(dc_heroes);
+// console.log(mixed); //returns length 
+// console.log(marvel_heroes); //returns array
+//It basically becomes array inside an array!! OR 2D array!!
+
+// console.log(marvel_heroes[4][0], marvel_heroes[4][3]);
+// //Accessing the elements of the 2D array!!
+// console.log(marvel_heroes[3][0], marvel_heroes[3][3]);
+//This will also give result, but it will go inside the 3rd element of the array
+//which is ironman and in that it will see the string's 0th and 3rd element and return!!
+
+//FOR ADDING THE ARRAY ELEMENTS ACTUALLY!!
+const m_heroes = ["Spiderman", "thor", "hulk", "ironman"];
+const d_heroes = ["batman", "flash", "superman", "wonderwoman"];
+
+const mixed_heroes = m_heroes.concat(d_heroes);
+//This is a temporary operation which means it returns a new array 
+//instead of changing the main array!! NON-MUTATING METHOD!!
+// console.log(mixed_heroes); 
+// console.log(m_heroes); //It doesn't change the main array!!
+
+//SPREAD OPERATOR
+const heroes = [...m_heroes, ...d_heroes];
+// console.log(heroes);
+
+//.flat() operator
+let nums = [1, 2, 3, 4, [5, 6, 7], [8, [9, 10], [11, 12]]];
+// console.log(nums.flat(1)); //THIS IS A TEMPORARY METHOD!! NON-MUTATING METHOD!!
+// console.log(nums.flat(2));
+// console.log(nums.flat(Infinity)); 
+// console.log(nums); //REMAINS SAME!!
+
+//.from() operation
+let str = "Surjo";
+console.log(Array.isArray("Surjo")); //THIS IS STATIC METHOS WHICH CHECKS IF ARRAY OR NOT!!
+console.log(Array.from(str)); //THIS IS TOO STATIC METHOD AND CHANGES OBJECT WHICH HAS LENGTH 
+//INTO AN ARRAY!!
